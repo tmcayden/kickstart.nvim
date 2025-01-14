4aa36b560c281cb27c6bf98c017fae12c0a0b631
Cayden Hunsaker <caydenh@goldsystems.com>
2025-01-14 08:24:20 -0700

    telescope niceities

--- init.lua
+++ init.lua
@@ -555,6 +555,7 @@ require('lazy').setup({
             path = vim.fn.stdpath 'data' .. '/telescope_history', -- Path to store history
             limit = 1000, -- Limit the number of saved search entries
           },
+          initial_mode = 'normal',
           mappings = {
             i = {
               -- Custom keybindings for interacting with search history
@@ -641,15 +642,6 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
