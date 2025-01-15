1e67e8f828664a19632422736d85062b0c59baa5
Cayden Hunsaker <caydenh@goldsystems.com>
2025-01-15 16:29:40 -0700

    ensure vue is formatted

--- init.lua
+++ init.lua
@@ -978,6 +978,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        vue = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
